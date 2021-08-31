71e664bbf55475bffeb44bf973102b8ca1822e19
ahlem9392 <abidiahlem11@gmail.com>
2021-08-31 08:33:56 +0100

    modification avec jquery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.getElementById('send').addEventListener('click',function(){
     var info=document.getElementById('info');
-     var firstName=firstName.value;
-     var lastName=lastName.value;
+     var firstName=$_POST['firstName'];
+     var lastName=$_POST['lastName'];
      info.innerHTML='Vous êtes Mme/Mrs '+lastName+''+firstName
 })
