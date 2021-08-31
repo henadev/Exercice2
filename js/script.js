b7523d092d728509d7ebe9f678fc774528ba95bb
ahlem9392 <abidiahlem11@gmail.com>
2021-08-31 08:26:07 +0100

    add js folder

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById('send').addEventListener('click',function(){
+    var info=document.getElementById('info');
+     firstName=document.getElementById('firstName'),
+     lastName=document.getElementById('lastName');
+     info.innerHTML='Vous êtes'+lastName+firstName
+})
