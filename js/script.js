d967794faca1f99d1c420fc566381c674aefb309
ahlem9392 <abidiahlem11@gmail.com>
2021-08-31 08:29:08 +0100

    ajout du contenue au code JS

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ document.getElementById('send').addEventListener('click',function(){
     var info=document.getElementById('info');
      firstName=document.getElementById('firstName'),
      lastName=document.getElementById('lastName');
-     info.innerHTML='Vous êtes'+lastName+firstName
+     info.innerHTML='Vous êtes Mme/Mrs '+lastName+''+firstName
 })
